1738d4989fd9a1ccd92612d00d193eac27982672
dashanteCEO <deshantestreet749@gmail.com>
2023-07-05 10:19:02 +0000

    g

--- src/components/auth/dashboard.js
+++ src/components/auth/dashboard.js
@@ -3,8 +3,26 @@ import axios from "axios";
 import "./dashboard.css";
 
 function Dashboard() {
-  const [vehicles, setVehicles] = useState([]);
+  const [vehicles, setVehicles] = useState([])
+  
   const [selectedGroup, setSelectedGroup] = useState(null);
+
+  useEffect(() => {
+    const fetchVehicles = async () => {
+        try {
+            const response = await fetch(`https://ddauto.up.railway.app/api/post/test`);
+            const data = await response.json();
+            if (response.ok) {
+              setVehicles(data.urls);
+            } else {
+              console.error("Error fetching files:", data);
+            }
+          } catch (error) {
+            console.error("Error fetching files:", error);
+          }
+        };
+        fetchVehicles();
+      }, [vehicles])
 
   useEffect(() => {
     if (selectedGroup) {
@@ -12,7 +30,7 @@ function Dashboard() {
         .delete(`https://ddauto.up.railway.app/api/post/delete/${selectedGroup}`)
         .then((res) => {
           console.log(res.data);
-          alert('Vehicle deleted!');
+          alert('Vehicle deleted!')
         })
         .catch((error) => {
           console.error(error);
@@ -20,54 +38,42 @@ function Dashboard() {
     }
   }, [selectedGroup]);
 
-  useEffect(() => {
-    const fetchVehicles = async () => {
-      try {
-        const response = await fetch(`https://ddauto.up.railway.app/api/post/test`);
-        const data = await response.json();
-        if (response.ok) {
-          setVehicles(data.urls);
-        } else {
-          console.error("Error fetching files:", data);
-        }
-      } catch (error) {
-        console.error("Error fetching files:", error);
-      }
-    };
-    fetchVehicles();
-  }, []);
-
-  const handleDelete = (groupId) => {
-    setSelectedGroup(groupId);
-  };
-
-  const token = localStorage.getItem('token');
-  if (!token) return null;
-  
-  return (
+      const handleDelete = (groupId) => {
+        setSelectedGroup(groupId);
+      };
+    
+      const token = localStorage.getItem('token')
+      if (!token) return null;
+  if (token) {
+    return (
     <div className="dashboard">
-      <div className="header">
-        <h2>Dashboard</h2>
-        <a
-          href="/post"
-          style={{
-            textDecoration: 'none',
-            textTransform: 'capitalize',
-            padding: '6px',
-            background: '#00c3ff',
-            color: '#fff',
-            fontWeight: '600',
-            borderRadius: '4px',
-            transition: 'all .4s ease',
-            textAlign: 'center'
-          }}
-        >
-          Post
-        </a>
-      </div>
-      <div className='cardss'>
-        {vehicles.map(item => (
-          <div key={item.groupId} className='card'>
+    <div className="header" >
+      <h2>Dashboard</h2>
+      <a
+  href="/post"
+  style={{
+    textDecoration: 'none',
+    textTransform: 'capitalize',
+    padding: '6px',
+    background: '#00c3ff',
+    color: '#fff',
+    fontWeight: '600',
+    borderRadius: '4px',
+    transition: 'all .4s ease',
+    textAlign: 'center'
+  }}
+>
+  Post
+</a>
+
+    </div>
+    <div className='cardss'>
+      {
+        vehicles.map(item=>{
+          return(
+            <div 
+            // to={`/vehicle/${item.groupId}`}
+             key={item.groupId} className='card'>
             <img src={item.url} alt={item.url} />
             <p>{item.brand}</p>
             <p className='model'>{item.model}</p>
@@ -76,10 +82,13 @@ function Dashboard() {
             <p>${item.price}</p>
             <button className="p" onClick={() => handleDelete(item.groupId)}>delete</button>
           </div>
-        ))}
+          )
+        })
+      }
       </div>
     </div>
   );
+}
 }
 
 export default Dashboard;
