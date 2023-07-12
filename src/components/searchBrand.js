6bd2a20ecce854075d0537ad08053bd59fccb590
dashanteCEO <deshantestreet749@gmail.com>
2023-07-12 17:28:06 +0000

    n

--- src/components/searchBrand.js
+++ src/components/searchBrand.js
@@ -31,7 +31,7 @@ export default function SearchBrand(){
           }
     }
     fetchVehicles();
-  },[currentPage,brand])
+  },[brand, currentPage])
  
   const handlePageChange = (page) => {
     setCurrentPage(page);
