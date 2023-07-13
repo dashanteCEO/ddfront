441cd823cf958b0df086efa5a97ff4e462a78aef
dashanteCEO <deshantestreet749@gmail.com>
2023-07-13 20:12:36 +0000

    c

--- src/components/homeText.js
+++ src/components/homeText.js
@@ -1,29 +1,25 @@
-export default Text = [
+const Text = [
     {
-        text : "Welcome to D and D Auto Ja, where your car dreams become a reality! As the premier car dealership in town, we are committed to providing an exceptional car buying experience that will leave you thrilled and satisfied. Here's why choosing D and D Auto Ja is the best decision you can make:"
+        text: "Welcome to D and D Auto Ja, where your car dreams become a reality! As the premier car dealership in town, we are committed to providing an exceptional car buying experience that will leave you thrilled and satisfied. Here's why choosing D and D Auto Ja is the best decision you can make:"
     },
-        
     {
-        text : "Vast Selection of Quality Vehicles: At D and D Auto Ja, we pride ourselves on offering an extensive inventory of top-notch vehicles. Whether you're looking for a sleek sedan, a powerful SUV, or a reliable family car, we have the perfect match for your needs and preferences. Our collection features vehicles from renowned brands, ensuring you have access to the latest models and cutting-edge technologies."
+        text: "Vast Selection of Quality Vehicles: At D and D Auto Ja, we pride ourselves on offering an extensive inventory of top-notch vehicles. Whether you're looking for a sleek sedan, a powerful SUV, or a reliable family car, we have the perfect match for your needs and preferences. Our collection features vehicles from renowned brands, ensuring you have access to the latest models and cutting-edge technologies."
     },
-    
     {
-        text : "Unparalleled Customer Service: We believe that exceptional customer service is the cornerstone of any successful business. Our friendly and knowledgeable sales team is dedicated to guiding you through every step of the car buying process. We take the time to understand your requirements, answer your questions, and provide valuable insights to help you make an informed decision. Your satisfaction is our utmost priority."
+        text: "Unparalleled Customer Service: We believe that exceptional customer service is the cornerstone of any successful business. Our friendly and knowledgeable sales team is dedicated to guiding you through every step of the car buying process. We take the time to understand your requirements, answer your questions, and provide valuable insights to help you make an informed decision. Your satisfaction is our utmost priority."
     },
-    
     {
-        text : "Competitive Pricing and Financing Options: We understand that purchasing a vehicle is a significant investment, which is why we offer competitive pricing and flexible financing options. Our transparent and fair pricing ensures that you get the best value for your money. Additionally, our finance experts are here to assist you in securing the most suitable financing solution tailored to your budget and credit profile."
+        text: "Competitive Pricing and Financing Options: We understand that purchasing a vehicle is a significant investment, which is why we offer competitive pricing and flexible financing options. Our transparent and fair pricing ensures that you get the best value for your money. Additionally, our finance experts are here to assist you in securing the most suitable financing solution tailored to your budget and credit profile."
     },
-    
     {
-        text : "Rigorous Vehicle Inspections and Warranties: Rest assured that every vehicle on our lot undergoes a thorough inspection by our certified technicians. We believe in delivering only the highest quality vehicles to our customers. To further instill confidence, we offer comprehensive warranties on our cars, providing you with peace of mind and protection against unexpected repairs."
+        text: "Rigorous Vehicle Inspections and Warranties: Rest assured that every vehicle on our lot undergoes a thorough inspection by our certified technicians. We believe in delivering only the highest quality vehicles to our customers. To further instill confidence, we offer comprehensive warranties on our cars, providing you with peace of mind and protection against unexpected repairs."
     },
-    
     {
-        text : "Exceptional After-Sales Support: Our commitment to your satisfaction extends beyond the point of purchase. We have a dedicated service center staffed with skilled technicians who are ready to address any maintenance or repair needs your vehicle may have. From routine services to major repairs, we are here to keep your car running smoothly and efficiently."
+        text: "Exceptional After-Sales Support: Our commitment to your satisfaction extends beyond the point of purchase. We have a dedicated service center staffed with skilled technicians who are ready to address any maintenance or repair needs your vehicle may have. From routine services to major repairs, we are here to keep your car running smoothly and efficiently."
     },
-    
     {
-        text : "At D and D Auto Ja, we strive to create a personalized and memorable car buying experience for each of our customers. Visit our showroom today and let us assist you in finding your dream car. Our team is eagerly waiting to welcome you and help you drive off in style. Discover the difference at D and D Auto Ja, where your satisfaction is our success!"
+        text: "At D and D Auto Ja, we strive to create a personalized and memorable car buying experience for each of our customers. Visit our showroom today and let us assist you in finding your dream car. Our team is eagerly waiting to welcome you and help you drive off in style. Discover the difference at D and D Auto Ja, where your satisfaction is our success!"
     }
-]
+];
+
+export default Text;
