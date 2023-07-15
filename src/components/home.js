5a6525b7c3259dbed0145a0c813fa6c7b49cfdcd
dashanteCEO <deshantestreet749@gmail.com>
2023-07-15 12:15:53 +0000

    j

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './css/home.css';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCar } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCar } from '@fortawesome/free-solid-svg-icons';
 import Text from './homeText';
 
 export default function Home() {
